cd1c2af901840896162f71ad8af2604707b3513e
Ruby on Rails Developer <serkanbozdemiir@gmail.com>
2018-08-05 17:58:36 +0000

    Deploy and wrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,20 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article,only:[:edit,:update,:show,:destroy]
   def index
     @article=Article.all
   end
-  
   def new
   @article = Article.new
   end
-
   def show
-    @article = Article.find(params[:id])
   end
-  
 def destroy
-
-@article = Article.find(params[:id])
-
 @article.destroy
-
 flash[:notice] = "Article was successfully deleted"
-
 redirect_to articles_path
-
 end
   def edit
-    @article=Article.find(params[:id])
   end
-  
   def create
   @article =Article.new(article_params)
   if @article.save
@@ -36,20 +24,18 @@ end
     render 'new'
   end
   end
-  
 def update
-
-@article = Article.find(params[:id])
 if @article.update(article_params)
 flash[:notice] = "Article was successfully updated"
 redirect_to article_path(@article)
 else
 render 'edit'
 end
-
 end
-  
    private
+   def set_article
+     @article = Article.find(params[:id])
+   end
    def article_params
    params.require(:article).permit(:title, :description)
    end
